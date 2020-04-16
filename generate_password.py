7efac0cbdc6b5a412a719243ee119455a691df96
Maxim Sinyakov <m.sinyakov@cian.ru>
2020-04-16 05:23:36 +0300

    generate_password function

--- generate_password.py
+++ generate_password.py
@@ -1,2 +1,14 @@
-def generate_password(length="12", withNumbers=None, withSymbols=None, excludeSimilarCharacters=None):
-    return length
+from string import ascii_lowercase
+from random import choice
+
+
+def generate_password(length="12", withCapitalLetters=None, withNumbers=None, withSymbols=None, excludeSimilarCharacters=None):
+    letters = set(ascii_lowercase)
+    capital_letters = set(ascii_lowercase.upper()) if withCapitalLetters is not None else set()
+    numbers = set(map(str, range(10))) if withNumbers is not None else set()
+    symbols = set("!@#$%^&*()") if withSymbols is not None else set()
+    similar_characters = set("iloILO10") if excludeSimilarCharacters is not None else set()
+
+    allowed_characters = list((letters | capital_letters | numbers | symbols) - similar_characters)
+    
+    return "".join(choice(allowed_characters) for _ in range(int(length)))
